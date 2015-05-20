db11d4718759ab0051d859e048804704c5c7bd4c
Syxxxx <Syxxxx@users.noreply.github.com>
2015-05-20 10:50:14 +0200

    Add sshsearcher.service

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 install ./uk.co.zygous.sshsearcher.desktop /usr/share/applications/
 install ./uk.co.zygous.sshsearcher.search-provider.ini /usr/share/gnome-shell/search-providers/
 install ./ssh-search-provider.js /usr/local/bin/
+install ./uk.co.zygous.sshsearcher.service /usr/share/dbus-1/services/
 
 echo "You may need to restart GNOME Shell to load the search provider."
 echo "Enable the provider via Control Centre under Search."
